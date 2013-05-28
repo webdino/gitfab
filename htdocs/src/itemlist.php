28fa6a3e61585cb818ac36bf2a582fef34f436c1
dadaa <daisuke@mozilla-japan.org>
2013-05-28 18:27:48 +0900

    item list

---
+++ htdocs/src/itemlist.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this file,
+   - You can obtain one at http://mozilla.org/MPL/2.0/.  -->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title data-l10n-id="title"></title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/common.css" type="text/css">
+    <link rel="stylesheet" href="css/itemlist.css" type="text/css">
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="js/itemlist.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+  </head>
+  <body>
+    <?php include('header.php.inc'); ?>
+    <?php include('toolbar.php.inc'); ?>
+    <div id="main" role="main">
+      <ul id="item-list">
+        <li class="item">
+          <a href="item.php">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+          </a>
+        </li>
+        <li class="item">
+          <a href="item.php?s">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+          </a>
+        </li>
+        <li class="item">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+        </li>
+        <li class="item">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+        </li>
+        <li class="item">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+        </li>
+        <li class="item">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+        </li>
+        <li class="item">
+          <div class="thumbnail"></div>
+          <ul class="meta">
+            <li class="title">タイトル</li>
+            <li class="abstract">時計は２４時間をはかるためだけのものではなく、むしろ自分に必要な時間感覚を１単位とする時計をつくる。</li>
+            <li class="author">作者</li>
+            <li class="date">作成日</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <?php include('footer.php.inc'); ?>
+  </body>
+</html>
