17cbdb2b781856282102d1fa218f5318d8c94958
dadaa <daisuke@mozilla-japan.org>
2013-06-07 01:04:22 +0900

    changes the parameters that send to item.php

--- htdocs/src/js/itemlist.js
+++ htdocs/src/js/itemlist.js
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var LIST_API = "https://api.github.com/users/gitfab/subscriptions?callback=?";
-var ITEM_PAGE = "item.php?item=";
+var ITEM_PAGE = "item.php?";
 
 var ItemListController = {
   init: function() {
@@ -15,10 +15,13 @@ var ItemListController = {
     var itemlist = result.data;
     for (var i = 0, n = itemlist.length; i < n; i++) {
       var item = itemlist[i];
+      var owner = item.owner.login;
+      var repository = item.name;
+      var master = item.master_branch;
       var li = $(document.createElement("li"));
       li.addClass("item");
       var link = $(document.createElement("a"));
-      link.attr("href", ITEM_PAGE+item.full_name);
+      link.attr("href", ITEM_PAGE+"owner="+owner+"&repository="+repository+"&master="+master);
       link.text(item.full_name);
       li.append(link);
       ul.append(li);
