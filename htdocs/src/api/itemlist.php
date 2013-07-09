43ddc03b2e9dcb991a8f11d361b4d976e4b84a4e
dadaa <daisuke@mozilla-japan.org>
2013-07-10 00:19:04 +0900

    cgi

---
+++ htdocs/src/api/itemlist.php
@@ -0,0 +1,14 @@
+<?php
+  $owner = $_GET["owner"];
+  $tag = $_GET["tag"];
+
+  include('local-database-functions.php.inc');
+  $result = array();
+  try {
+    $result["itemlist"] = getItemList($owner, $tag);
+  } catch (Exception $e) {
+    $result["error"] = $e;
+  }
+
+  echo json_encode($result);
+?>
