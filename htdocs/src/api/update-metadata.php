e82bbc955e35ba2b91c2f8d5fff5f72849136d07
dadaa <daisuke@mozilla-japan.org>
2013-07-10 00:19:40 +0900

    avatar and thumbnail

--- htdocs/src/api/update-metadata.php
+++ htdocs/src/api/update-metadata.php
@@ -3,11 +3,13 @@
   $repository = $_GET["repository"];
   $oldrepository = $_GET["oldrepository"];
   $tags = $_GET["tags"];
+  $avatar = $_GET["avatar"];
+  $thumbnail = $_GET["thumbnail"];
 
   include('local-database-functions.php.inc');
   $result = array();
   try {
-    updateRepository($owner, $repository, $oldrepository, $tags);
+    updateRepository($owner, $repository, $oldrepository, $tags, $avatar, $thumbnail);
     $result["ok"] = "ok";
   } catch (Exception $e) {
     $result["error"] = $e;
