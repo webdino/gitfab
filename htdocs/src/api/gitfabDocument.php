757152708b9271c865628738bf8c7ded9c864b67
hrl7 <hrlclb@gmail.com>
2013-11-27 16:35:34 +0900

    remove htmltags in readme.md without video tag

--- htdocs/src/api/gitfabDocument.php
+++ htdocs/src/api/gitfabDocument.php
@@ -1,7 +1,8 @@
 <?php
-  $owner = $_GET["owner"];
-  $repository = $_GET["repository"];
-  $branch = $_GET["branch"];
+  $owner = htmlspecialchars($_GET["owner"],ENT_QUOTES);
+  $repository = htmlspecialchars($_GET["repository"],ENT_QUOTES);
+  $branch = htmlspecialchars($_GET["branch"],ENT_QUOTES);
+  require_once('lib/htmlpurifier/library/HTMLPurifier.auto.php');
 
   $url = "https://raw.github.com/".$owner."/".$repository."/".$branch."/README.md?".time();
   $ch = curl_init();
@@ -12,6 +13,27 @@
   curl_setopt($ch, CURLOPT_FAILONERROR, true);
   curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
   $contents = curl_exec($ch);
+
+ 
+  $config = HTMLPurifier_Config::createDefault();
+  $config->set('Core.Encoding', 'UTF-8');
+  $config->set('Core.Language', 'ja');
+  $config->set('HTML.AllowedElements', array('video'));
+  $def = $config->getHTMLDefinition(true);
+  $video = $def->addElement(
+    'video',
+    'Block', 
+    'Empty', 
+    'Core', 
+    array( 
+      'src*' => 'URI',
+      'poster' => 'URI',
+	   'controls' => 'Bool')
+    );
+  $video->excludes = array('video' => true);
+
+  $hp = new HTMLPurifier($config);
+  $contents = $hp->purify($contents);
 
   if(curl_errno($ch)) {
     echo "";
