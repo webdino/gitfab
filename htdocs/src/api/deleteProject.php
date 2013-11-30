0a7a357de3b19d135afd370b09e644d2e9628486
hrl7 <hrlclb@gmail.com>
2013-11-30 15:32:30 +0900

    separated newProj and deleteProj from localDBfuncs

--- htdocs/src/api/deleteProject.php
+++ htdocs/src/api/deleteProject.php
@@ -9,9 +9,25 @@
   include('localDatabaseFunctions.php.inc');
   $result = array();
   try {
-    $result = deleteProject($owner,$repository,$branch);
-  } catch (Exception $e) {
-    $result["error"] = $e->getMessage();
+    $connection = openConnection();
+    $connection -> beginTransaction();
+ 
+    $id = -1;
+    $id = findProject($owner, $repository, $branch, $connection);
+      $query = "DELETE FROM tags WHERE repository_id=?";
+      $statement = $connection -> prepare($query);
+      $statement -> execute(array($id));
+      $query = "DELETE FROM repositories WHERE id=?";
+      $statement = $connection -> prepare($query);
+      $statement -> execute(array($id));
+ 
+      $connection -> commit();
+      closeConnection($connection);
+      $result = $statement;
+    } 
+    catch (PDOException $e) {
+      $connection -> rollBack();
+      $result["error"] = $e->getMessage();
   }
   echo json_encode($result);
 ?>
