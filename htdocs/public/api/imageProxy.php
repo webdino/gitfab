c7f5947ae0a9a0876509764720cc226d8dcca608
dadaa <daisuke@mozilla-japan.org>
2013-12-13 08:55:44 +0900

    #77

--- htdocs/public/api/imageProxy.php
+++ htdocs/public/api/imageProxy.php
@@ -1,5 +1,9 @@
 <?php
   $url = $_GET['url'];
   $url = rawurldecode($url);
+  preg_match("/([^\/]+)$/", $url, $matches);
+  $originalFileName = $matches[0];
+  $filename = urlencode($originalFileName);
+  $url = str_replace($originalFileName, $filename, $url);
   echo file_get_contents($url);
 ?>
