2494bde2b5f0ae65c05d75a3f415f3966eb0a1f9
dadaa <daisuke@mozilla-japan.org>
2013-12-16 21:49:31 +0900

    add new file

--- htdocs/Gruntfile.js
+++ htdocs/Gruntfile.js
@@ -142,6 +142,7 @@ module.exports = function(grunt) {
 				 '<%= dirs.src.css%>/logger.css',
 				 '<%= dirs.src.css%>/slide.css',
 				 '<%= dirs.src.css%>/project.css',
+				 '<%= dirs.src.css%>/colorbox.css',
 				 '<%= dirs.src.css%>/projectEditor.css'
 				 ],
 			dest:'<%= dirs.dest.css%>/project.min.css'
